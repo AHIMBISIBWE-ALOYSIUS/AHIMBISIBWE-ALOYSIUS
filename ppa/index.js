2a6925dee81b2e77ceb270e533d8997c3c3ce23f
AHIMBISIBWE-ALOYSIUS <143800208+AHIMBISIBWE-ALOYSIUS@users.noreply.github.com>
2023-12-11 10:07:07 +0300

    Add files via upload

---
+++ ppa/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".front-message").fadeIn(1000);
+});
+
+$(document).ready(function(){
+    $(".hobby").fadeIn(1000);
+})
+
+function addMo() {
+    document.getElementById("addMo").innerHTML
+    += "<span> add more </span><br>"
+}
